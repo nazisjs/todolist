080be95e8ddc70698126af4693cd2dd0fbb68b63
nazerke <mnazerke89@gmail.com>
2025-09-29 19:59:08 +0500

    Modifications to login/registration

--- myproject/todo/urls.py
+++ myproject/todo/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path("register/",views.registerPage,name="register"),
-    path("login/",views.loginPage,name="login")
+    path("login/",views.loginPage,name="login"),
+    path("logout/",views.logoutUser,name="logout"),
+    path("",views.home,name="home"),
 ]
