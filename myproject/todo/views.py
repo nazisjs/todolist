1b3ddb4bac68bfff18d5f3b011010cbb1ea5bd1e
nazerke <mnazerke89@gmail.com>
2025-09-29 14:42:19 +0500

    Added registration

--- myproject/todo/views.py
+++ myproject/todo/views.py
@@ -1,3 +1,16 @@
+from .forms import CreateUserForm
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+def registerPage(request):
+    form=CreateUserForm()
+    if request.method=="POST":
+        form=CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context={'form':form}
+    return render(request,'todo_folder/register.html',context)
+
+def loginPage(request):
+    return render(request,'todo_folder/login.html')
