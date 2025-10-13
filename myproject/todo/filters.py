8a810486c4fd7c0ca6baa7c44faaf87a2b8c86db
nazerke <mnazerke89@gmail.com>
2025-10-13 09:17:13 +0500

    Fixed login url

--- myproject/todo/filters.py
+++ myproject/todo/filters.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 class TaskFilter(django_filters.FilterSet):
     status=django_filters.ChoiceFilter(choices=Task.CHOICE_STATUS)
-    expired_date=django_filters.BooleanFilter(method="filter_expired",label="Expired deadline")
+    expired_date=django_filters.ChoiceFilter(choices=[("Yes","Expired tasks"),("No","Not expired tasks")],method="filter_expired",label="Expired deadline")
     deadline_time=django_filters.DateFromToRangeFilter(field_name="deadline",label="Deadline",widget=django_filters.widgets.RangeWidget(
         attrs={"type":"date"}
     ),
@@ -15,6 +15,8 @@ class TaskFilter(django_filters.FilterSet):
         fields=['status']
     
     def filter_expired(self,queryset,name,value):
-        if value:
+        if value == "Yes":
             return queryset.filter(deadline__lt=timezone.now())
+        elif value=="No":
+            return queryset.filter(deadline__gte=timezone.now())
         return queryset
